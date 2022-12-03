9965ebf8aa1a613b86a0023fc3ea0bcb078249da
kardelen98 <kardelen.koc@hotmail.com>
2022-12-03 07:19:13 +0000

    Point system started

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //global variables
 let compChoice = "";
 let userInput = "";
+let playerPoints = 0;
+let computerPoints = 0;
 
 function computerPlay() {
     let compInput = Math.floor((Math.random() * 3) + 1);
@@ -18,7 +20,6 @@ function computerPlay() {
 }
 
 function userPlay() {
-    //get user input
     userInput = prompt("Rock, Paper or Scissors?");
 
     userInput = userInput.toLowerCase();
@@ -29,19 +30,35 @@ function round(player, computer) {
     //tie
     if (player == computer) {
         return "it's a tie";
-    }
+    } //rock > scissors conditions
     else if (player == "rock" && computer == "scissors") {
+        playerPoints++;
         return "You win! Rock beats Scissors!";
     }
     else if (player == "scissors" && computer == "rock") {
+        computerPoints++;
         return "You lose! Rock beats Scissors!";
-
+    }//scissors > paper
+    else if (player == "scissors" && computer == "paper") {
+        playerPoints++;
+        return "You win! Scissors beats Paper!";
     }
-
-
-    //player wins
+    else if (player == "paper" && computer == "scissors") {
+        computerPoints++;
+        return "You lose! Scissors beats Paper!";
+    }//paper > rock conditions
+    else if (player == "paper" && computer == "rock") {
+        playerPoints++;
+        return "You win! Paper beats Rock!";
+    }
+    else if (player == "rock" && computer == "paper") {
+        computerPoints++;
+        return "You lose! Paper beats Rock!";
+    } else {
+        return "what?";
+    }
 }
 
-console.log(computerPlay());
-console.log(userPlay());
+computerPlay();
+userPlay();
 console.log(round(userInput, compChoice));

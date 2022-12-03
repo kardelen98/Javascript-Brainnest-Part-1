1e237310751e6e200471db884b8cd510d634813a
kardelen98 <kardelen.koc@hotmail.com>
2022-12-03 07:01:44 +0000

    Tie works

--- script.js
+++ script.js
@@ -24,38 +24,17 @@ function userPlay() {
     userInput = userInput.toLowerCase();
     return userInput;
 }
-
-function compare(a, b) {
-    //computer wins ?
-    if (a === b) {
-        return "It's a Tie";
-    }
-    //if player wins
-    else if (a === "rock" && b === "scissors") {
-        return "You win! Rock beats Scissors";
-    } else if (a == "paper" && b == "rock") {
-        return "You win! Paper beats Rock";
-    } else if (a == "scissors" && b == "paper") {
-        return "You win! Scissors beats Paper";
-    }
-    //if computer wins
-    else if (b == "rock" && a == "scissors") {
-        return "You lose! Rock beats Scissors";
-    } else if (b == "paper" && a == "rock") {
-        return "You lose! Paper beats Rock";
-    } else if (b == "scissors" && a == "paper") {
-        return "You lose! Scissors beats Paper";
-    }
-    //rock > scissors
-    //paper > rock
-    //scissors >paper
+function round(player, computer) {
     //tie
+    if (player == computer) {
+        return "it's a tie";
+    }
 
+    //computer wins
+
+    //player wins
 }
 
 console.log(computerPlay());
-computerPlay();
 console.log(userPlay());
-
-// console.log(userPlay());
-console.log(compare(userInput, compChoice));
+console.log(round(userInput, compChoice));
